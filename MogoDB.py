7d7a5c86063fc42c1f19cbd1079c34ddfebdff3a
Purestream <github@iqaq.cc>
2024-11-24 13:56:26 +0800

    update MogoDB.py

--- MogoDB.py
+++ MogoDB.py
@@ -1,4 +1,5 @@
 import pymongo
+import pandas as pd
 
 class MongoDBClient:
     def __init__(self, user, password, server, db_name):
@@ -18,16 +19,20 @@ class MongoDBClient:
             return {}
                 #_name: name string, description: des
     def get_df(self, name):
-        # get the db using string; 
-        #table 
-        #a, b, c, d; 
-        # a1, b1, c1, d1
-        # a2, b2, 
+        collections = self.db.list_collection_names()
+        data = []
 
-        # {a:[a1, a2, ....]}
-        
-        
-        # return df
+        for col in collections:
+            for line in self.db[col].find({"line": {"$regex": name}}):
+                id = line["_id"]
+                content = line["line"]
+                data.append({"collection": col, "id": id, "content": content})
+
+        df = pd.DataFrame(data, columns=["collection", "id", "content"])
+        return df
+                
+
+
     
 
 # Example usage
@@ -37,5 +42,7 @@ server = "154.208.10.3"
 db_name = "4011"
 
 mongo_client = MongoDBClient(user, password, server, db_name)
-mongo_client.list_collections()
-mongo_client.search_text("inventory")
+#mongo_client.list_collections()
+#mongo_client.search_text("inventory")
+df = mongo_client.get_df("inventory")
+print(df)
